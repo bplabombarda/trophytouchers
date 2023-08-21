e78810c2a2fcf5548e70c8a50ef1af43251d26d7
Brett LaBombarda <blabombarda@arrivelogistics.com>
2023-08-20 22:15:06 -0500

    Do it again but with Vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import FinalsList from './components/FinalsList.vue'
+import Legend from './components/Legend.vue'
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Trophy Touchers</h1>
+    </header>
+
+    <FinalsList />
+    <Legend />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 24px;
+  margin: 0;
+  text-transform: uppercase;
+}
+</style>
---
+++ src/components/FinalsList.vue
@@ -0,0 +1,36 @@
+<script>
+import FinalsListItem from './FinalsListItem.vue'
+import json from '../data.json'
+
+export default {
+  components: {
+    'finals-list-item': FinalsListItem
+  },
+  data() {
+    return {
+      finals: json.data
+    }
+  },
+}
+</script>
+
+<template>
+  <ul id="finals-list">
+    <li className="finals-list-header">
+      <div>Year</div>
+      <div>Campbell</div>
+      <div>Wales</div>
+    </li>
+    <finals-list-item v-for="final in finals" :final="final" />
+  </ul>
+</template>
+
+<style scoped>
+#finals-list {
+  padding: 0;
+}
+
+.finals-list-header {
+  opacity: 0.5;
+}
+</style>
